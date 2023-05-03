2920ffbd8197a9beb9b5520bd219dd51215faa4c
Erik Pihl <pihl@primea.se>
2023-05-03 10:56:21 +0200

    Adds test utils for databases

--- database/dummy.go
+++ database/dummy.go
@@ -7,7 +7,15 @@ import (
 	"github.com/jackc/pgx/v4/pgxpool"
 )
 
+type Resp struct {
+	Data interface{}
+	Err  error
+}
 type Dummy struct {
+	Responses     []*Resp
+	QueryCalls    int
+	QueryRowCalls int
+	ExecCalls     int
 }
 
 func NewDummy() *Dummy {
@@ -15,23 +23,43 @@ func NewDummy() *Dummy {
 }
 
 func (d *Dummy) Ping(ctx context.Context) error {
-	panic("not implemented")
+	panic("not implemented ping")
 }
 func (d *Dummy) Query(ctx context.Context, sql string, args ...interface{}) (pgx.Rows, error) {
-	panic("not implemented")
+	d.QueryCalls++
+	r := d.Responses[0]
+	d.Responses = append(d.Responses[1:])
+	if r.Data != nil {
+		return r.Data.(pgx.Rows), nil
+	} else {
+		return nil, r.Err
+	}
 }
 func (d *Dummy) QueryRow(ctx context.Context, sql string, args ...interface{}) pgx.Row {
-	panic("not implemented")
+	d.QueryRowCalls++
+	r := d.Responses[0]
+	d.Responses = append(d.Responses[1:])
+	if r.Data == nil {
+		panic("Query row needs data")
+	}
+	return r.Data.(pgx.Row)
 }
 func (d *Dummy) Exec(ctx context.Context, sql string, args ...interface{}) (pgconn.CommandTag, error) {
-	panic("not implemented")
+	d.ExecCalls++
+	r := d.Responses[0]
+	d.Responses = append(d.Responses[1:])
+	if r.Data != nil {
+		return r.Data.([]byte), nil
+	} else {
+		return nil, r.Err
+	}
 }
 func (d *Dummy) BeginTx(ctx context.Context, txOptions pgx.TxOptions) (pgx.Tx, error) {
-	panic("not implemented")
+	panic("not implemented begin")
 }
 func (d *Dummy) Pool() *pgxpool.Pool {
-	panic("not implemented")
+	panic("not implemented pool")
 }
 func (d *Dummy) Close() {
-	panic("not implemented")
+	panic("not implemented close")
 }
